ccadcddbb80d9fe9334722a0ac59e6b25a1537cf
Washington Junior <washington@MacBook-Pro-de-Washington.local>
2020-12-04 15:29:32 -0300

    update: api url

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const withImages = require('next-images');
 module.exports = withImages({
   esModule: true,
   env: {
-    apiURL: 'http://200.20.113.53:5000',
+    apiURL: '//200.20.113.53:5000',
   },
 });
